0736e884bfccfdd95f380c033728ea2523568e7b
Ibrahim <ibs_7867@hotmail.com>
2020-01-14 17:12:36 +0000

    Initial Commit

---
+++ FitSpace/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitSpace
+//
+//  Created by Ibrahim Hussain on 14/01/2020.
+//  Copyright © 2020 Ibrahim Hussain. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
